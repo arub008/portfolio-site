ecf68ac9dde0e9f8aa92bcbafff536f09db5baad
Anastasia Rubtsova <arub008@gmail.com>
2016-03-10 11:19:18 +0300

    improve work section

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 $(function() {
 	smoothScroll(400);
+	workBelt();
 });
 
 // smoothScroll function is applied from the document ready function
@@ -15,4 +16,18 @@ function smoothScroll (duration) {
 	        }, duration);
 	    }
 	});
+}
+
+function workBelt() {
+
+  $('.thumb-unit').click(function(){
+		$('.work-belt').css('left', '-100%');
+		$('.work-container').show();
+	} );
+
+	$('.work-return').click(function(){
+		$('.work-belt').css('left', '0%');
+		$('.work-container').hide(800);
+	});
+
 }
